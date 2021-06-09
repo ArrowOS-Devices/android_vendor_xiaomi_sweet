5d9e8028e84430a7d41a5cefecacdbe147d1cf08
daniml3 <danimoral1001@gmail.com>
2021-06-09 18:24:27 +0200

    Update blobs from MIUi 12.5.3.0
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Ic003671e27b1ef9a097ddf9065aac8972a2ff360

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:bf150c37ffa413b4d441291b4cc663b03bfbb825; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:257c5483c0f1da7aff9fb90ded3eb69a43c5bc2b; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:73395840d367378901e2c7b1448c8a8e6436e079 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:bf150c37ffa413b4d441291b4cc663b03bfbb825 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:2fc9a0addf3d5fe925b442a38be6de30bc509d68 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:257c5483c0f1da7aff9fb90ded3eb69a43c5bc2b && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
