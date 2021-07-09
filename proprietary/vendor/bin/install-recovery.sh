58c75567f5c76df88000acccd1dcf93547abadf1
daniml3 <daniel@danielml.dev>
2021-07-09 23:31:37 +0200

    Update blobs from MIUI v12.5.4.0
    * Also remove some prebuilt shared libraries
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I165102f5cf242a9f47cb1b746ae075778ae0844d

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:257c5483c0f1da7aff9fb90ded3eb69a43c5bc2b; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:e2da06f6819ed4f78a17836c0cec5f41ac8a437a; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:2fc9a0addf3d5fe925b442a38be6de30bc509d68 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:257c5483c0f1da7aff9fb90ded3eb69a43c5bc2b && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:182b72d8e353e932e64a2dd3e0b73c377ed24c83 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:e2da06f6819ed4f78a17836c0cec5f41ac8a437a && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
