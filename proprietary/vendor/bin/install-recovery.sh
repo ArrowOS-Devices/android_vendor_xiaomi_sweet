577ca12f09dab7bc0575c9d7879b3980fac1bbec
daniml3 <danimoral1001@gmail.com>
2021-08-08 01:48:32 +0200

    Sync with the device tree
    * Remove unneeded files at vendor/bin
    * Import audio_platform_info_intcodec to the device tree
    * Unpin acdbdata
    * Update blobs from MIUI V12.5.6.0 RKFMIXM
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I2d263e365bf9e084d2240db95256a08fd4123347

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:e2da06f6819ed4f78a17836c0cec5f41ac8a437a; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:99dc4051106cb5f161b4d0c2defea5aa6c1d2a42; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:182b72d8e353e932e64a2dd3e0b73c377ed24c83 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:e2da06f6819ed4f78a17836c0cec5f41ac8a437a && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:9c86df38b4cef30b15873ab693b34adae085a678 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:99dc4051106cb5f161b4d0c2defea5aa6c1d2a42 && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
