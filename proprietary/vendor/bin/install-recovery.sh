ef0af7b2e814dc0646176257dcf1efc1610a6e19
daniml3 <daniel@danielml.dev>
2021-09-26 10:49:33 +0200

    Update blobs from MIUI v12.5.8.0 RKFMIXM
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I6d19432bdef7b507c492839669843af0caeac102

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:99dc4051106cb5f161b4d0c2defea5aa6c1d2a42; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:a480d751110e5476fefdf4ed0ec642e94d075dd7; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:9c86df38b4cef30b15873ab693b34adae085a678 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:99dc4051106cb5f161b4d0c2defea5aa6c1d2a42 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:8586071402183f841f60c659930f99cfd5789350 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:a480d751110e5476fefdf4ed0ec642e94d075dd7 && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
